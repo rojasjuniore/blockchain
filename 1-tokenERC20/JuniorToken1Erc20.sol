41049ee03465c45eabcac264f74cc7a4141838f4
Junior Rojas <yunior0000@gmail.com>
2021-08-17 18:42:25 -0500

    correciones varias

--- 1-tokenERC20/JuniorToken1Erc20.sol
+++ 1-tokenERC20/JuniorToken1Erc20.sol
@@ -1,154 +1,186 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.4 <0.7.0;
-pragma experimental ABIEncoderV2;
-import "./SafeMath.sol";
+pragma solidity ^0.4.24;
 
-// Junior Rojas: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-// Jose Perez: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
-// Maria Santos: 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c
+// ----------------------------------------------------------------------------
+// Sample token contract
+//
+// Symbol        : JT1
+// Name          : Junior Token 1
+// Total supply  : 100000
+// Decimals      : 2
+// Owner Account : 0x4C54d42aB8a14E0142df679a075E4C4dE767d8D0
+//
+// Enjoy.
+//
+// (c) by Juan Cruz Martinez 2020. MIT Licence.
+// ----------------------------------------------------------------------------
 
-interface IRC20 {
-    // devuelve la cantidad de tokens en existencia
-    function totalSupply() external view returns (uint256);
 
-    // devuelve la cantidad de tokens para una direccion indicada por parametros
-    function balanceOf(address account) external view returns (uint256);
+// ----------------------------------------------------------------------------
+// Lib: Safe Math
+// ----------------------------------------------------------------------------
+contract SafeMath {
 
-    // devuelve el numero de token que el spender podra gastar en nombrede propetario de la direccion
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
 
-    // devuelve un valor boleanos resultado de la operacion de compra
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
 
-    // devuelve un valor boleanos resultado de la operacion de gasto
-    function approve(address spender, uint256 amount) external returns (bool);
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
 
-    // devuelve un valor boleanos resultado de la operacion de una cantida de tokens usando el metodo de allowance()
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    // evento que se debe emitir cuando una cantidad de tokens pase de un origen a un destino
-    event Tranfer(address indexed from, address indexed to, uint256 value);
-
-    // Evento que se debe emitir cuando se establece una asignacion con el metodo allowance()
-    event Approve(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
 }
 
-contract ERCBasic is IRC20 {
-    string public constant name = "Junior Token 2";
-    string public constant symbol = "JT2";
-    string public constant decimal = "18";
 
-    event Tranfer(address indexed from, address indexed to, uint256 tokens);
+/**
+ERC Token Standard #20 Interface
+https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+*/
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-    event Approve(
-        address indexed owner,
-        address indexed spender,
-        uint256 tokens
-    );
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
 
-    using SafeMath for uint256;
 
-    mapping(address => uint256) balances;
-    mapping(address => mapping(address => uint256)) allowed;
+/**
+Contract function to receive approval and execute function in one call
+Borrowed from MiniMeToken
+*/
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
 
-    uint256 totalSupply_;
+/**
+ERC20 Token, with the addition of symbol, name and decimals and assisted token transfers
+*/
+contract JT1Token is ERC20Interface, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
 
-    constructor(uint256 initialSupply) public {
-        totalSupply_ = initialSupply;
-        balances[msg.sender] = totalSupply_;
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+
+    // ------------------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------------------
+    constructor() public {
+        symbol = "JT1";
+        name = "Junior Token 1";
+        decimals = 2;
+        _totalSupply = 100000;
+        balances[0x4C54d42aB8a14E0142df679a075E4C4dE767d8D0] = _totalSupply;
+        emit Transfer(address(0), 0x4C54d42aB8a14E0142df679a075E4C4dE767d8D0, _totalSupply);
     }
 
-    function totalSupply() public view override returns (uint256) {
-        return totalSupply_;
+
+    // ------------------------------------------------------------------------
+    // Total supply
+    // ------------------------------------------------------------------------
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
     }
 
-    function increaseTotalSupply(uint256 newTokenAmout) public {
-        totalSupply_ += newTokenAmout;
-        balances[msg.sender] += newTokenAmout;
-    }
 
-    function balanceOf(address tokenOwner)
-        public
-        view
-        override
-        returns (uint256)
-    {
+    // ------------------------------------------------------------------------
+    // Get the token balance for account tokenOwner
+    // ------------------------------------------------------------------------
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
         return balances[tokenOwner];
     }
 
-    function allowance(address owner, address delegate)
-        public
-        view
-        override
-        returns (uint256)
-    {
-        return allowed[owner][delegate];
-    }
 
-    function transfer(address recipient, uint256 numTokens)
-        public
-        override
-        returns (bool)
-    {
-        // verificamos que tenga los token que quieren enviar
-        require(numTokens <= balances[msg.sender]);
-
-        // le restamos los token a que envia
-        balances[msg.sender] = balances[msg.sender].sub(numTokens);
-
-        // le agregamos lo token a los que reciben
-        balances[recipient] = balances[recipient].add(numTokens);
-
-        // emitimos el evento de tranfer cuando l transacion es exitosa
-        emit Tranfer(msg.sender, recipient, numTokens);
-
+    // ------------------------------------------------------------------------
+    // Transfer the balance from token owner's account to to account
+    // - Owner's account must have sufficient balance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
         return true;
     }
 
-    function approve(address delegate, uint256 numTokens)
-        public
-        override
-        returns (bool)
-    {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approve(msg.sender, delegate, numTokens);
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account
+    //
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+    // recommends that there are no checks for the approval double-spend attack
+    // as this should be implemented in user interfaces 
+    // ------------------------------------------------------------------------
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
         return true;
     }
 
-    function transferFrom(
-        address owner,
-        address buyer,
-        uint256 numTokens
-    ) public override returns (bool) {
-        // verificamos que tenga los token que quieren enviar
-        require(numTokens <= balances[owner]);
-        require(numTokens <= allowed[owner][msg.sender]);
 
-        // le restamos los token al verdedor
-        balances[owner] = balances[owner].sub(numTokens);
-
-        // me lo quito a mi como intermediario de la venta
-        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
-
-        // le agrego los token al comprador
-        balances[buyer] = balances[buyer].add(numTokens);
-
-        // emitimos los cambios
-        emit Tranfer(owner, buyer, numTokens);
-
+    // ------------------------------------------------------------------------
+    // Transfer tokens from the from account to the to account
+    // 
+    // The calling account must already have sufficient tokens approve(...)-d
+    // for spending from the from account and
+    // - From account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfers are allowed
+    // ------------------------------------------------------------------------
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
         return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be
+    // transferred to the spender's account
+    // ------------------------------------------------------------------------
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account. The spender contract function
+    // receiveApproval(...) is then executed
+    // ------------------------------------------------------------------------
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Don't accept ETH
+    // ------------------------------------------------------------------------
+    function () public payable {
+        revert();
     }
 }
